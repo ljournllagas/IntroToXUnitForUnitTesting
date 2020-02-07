d507d2d398c15daf836104cb8779a29bfb5c6461
Ljourn Llagas <40813696+ljournllagas@users.noreply.github.com>
2020-02-07 13:40:04 +0800

    Added a way to Categorize Test, Skip some test, and share contexts

--- GameEngine.Tests/BossEnemyShould.cs
+++ GameEngine.Tests/BossEnemyShould.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace GameEngine.Tests
 {
     public class BossEnemyShould
     {
+        private readonly ITestOutputHelper _output;
+
+        public BossEnemyShould(ITestOutputHelper output)
+        {
+            _output = output;
+        }
 
         #region "Floating Point Assert"
         [Fact]
+        [Trait("Category","Enemy")]
         public void HaveCorrectPower()
         {
+            _output.WriteLine("Creating Boss Enemy");
             BossEnemy sut = new BossEnemy();
 
             Assert.Equal(166.667, sut.TotalSpecialAttackPower, 3);
